8d910906eb6371d5dfb598ae8db8846f75299f11
Kyle Corry <kylecorry31@gmail.com>
2017-12-16 19:52:27 -0500

    Add linux support

--- src/main/java/com/kylecorry/stargazer/OpenCVManager.java
+++ src/main/java/com/kylecorry/stargazer/OpenCVManager.java
@@ -8,14 +8,30 @@ import java.io.File;
 public class OpenCVManager {
 
     public static void load() {
-        try {
-            System.load(new File("../lib/opencv_java320.dll").getAbsolutePath());
-        } catch (UnsatisfiedLinkError e) {
+        String os = System.getProperty("os.name");
+
+        if (os.equalsIgnoreCase("linux")) {
             try {
-                System.load(new File("libs/opencv_java320.dll").getAbsolutePath());
-            } catch (UnsatisfiedLinkError e1) {
-                System.err.println("Could not load opencv - FATAL");
-                System.exit(1);
+                System.load(new File("../lib/libopencv_java320.so").getAbsolutePath());
+            } catch (UnsatisfiedLinkError e) {
+                try {
+                    System.load(new File("libs/libopencv_java320.so").getAbsolutePath());
+                } catch (UnsatisfiedLinkError e1) {
+                    System.err.println("Could not load opencv - FATAL");
+                    System.exit(1);
+                }
+            }
+        } else {
+
+            try {
+                System.load(new File("../lib/opencv_java320.dll").getAbsolutePath());
+            } catch (UnsatisfiedLinkError e) {
+                try {
+                    System.load(new File("libs/opencv_java320.dll").getAbsolutePath());
+                } catch (UnsatisfiedLinkError e1) {
+                    System.err.println("Could not load opencv - FATAL");
+                    System.exit(1);
+                }
             }
         }
     }
