e240500f440cbf58322cbff8d4b74718357c16c2
Kyle Corry <kylecorry31@gmail.com>
2018-01-01 17:35:53 -0500

    Add style to filter settings description text

--- src/main/java/com/kylecorry/stargazer/ui/FilterSettingsController.java
+++ src/main/java/com/kylecorry/stargazer/ui/FilterSettingsController.java
@@ -77,6 +77,7 @@ public class FilterSettingsController implements Initializable {
             settingsPane.getChildren().add(sliderLabel);
             Label sliderLabelDesc = new Label();
             sliderLabelDesc.setText(currentSetting.getDescription());
+            sliderLabelDesc.getStyleClass().add("normal-text");
             settingsPane.getChildren().add(sliderLabelDesc);
             JFXSlider slider = new JFXSlider();
             slider.setMin(currentSetting.getMinValue());
