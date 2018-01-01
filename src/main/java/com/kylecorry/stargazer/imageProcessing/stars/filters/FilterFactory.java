4103e7b6c49d486f4d7008dbd9618d9cd81fe931
Kyle Corry <kylecorry31@gmail.com>
2018-01-01 17:49:22 -0500

    Add new filters

--- src/main/java/com/kylecorry/stargazer/imageProcessing/stars/filters/FilterFactory.java
+++ src/main/java/com/kylecorry/stargazer/imageProcessing/stars/filters/FilterFactory.java
@@ -9,12 +9,16 @@ public class FilterFactory {
             return new NoFilter();
         } else if (name.equalsIgnoreCase("background subtraction filter")) {
             return new BackgroundSubtractionFilter();
+        } else if (name.equalsIgnoreCase("Binary Sparse Luminosity Reduction Filter")) {
+            return new BinarySparseLuminosityReductionFilter();
+        } else if(name.equalsIgnoreCase("Brightness Filter")) {
+            return new BrightnessFilter();
         } else {
             return new SparseLuminosityReductionFilter();
         }
     }
 
     public List<IFilter> getAllFilters(){
-        return Arrays.asList(new SparseLuminosityReductionFilter(), new BackgroundSubtractionFilter(), new NoFilter());
+        return Arrays.asList(new SparseLuminosityReductionFilter(), new BinarySparseLuminosityReductionFilter(), new BackgroundSubtractionFilter(), new BrightnessFilter(), new NoFilter());
     }
 }
