13a4977d45e5a4f4df5e4a671eb04548eee22b96
Kyle Corry <kylecorry31@gmail.com>
2018-01-02 10:32:03 -0500

    Add icon

--- src/main/java/com/kylecorry/stargazer/Main.java
+++ src/main/java/com/kylecorry/stargazer/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -25,6 +26,7 @@ public class Main extends Application {
         primaryStage.setTitle("Stargazer");
         scene.getStylesheets().add("/styles/styles.css");
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/icon.png")));
         primaryStage.show();
     }
 }
